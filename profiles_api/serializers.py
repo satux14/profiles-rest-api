204d99d6313018c809c7b797e436fe6e7d6fba1a
Sathish <sathishkumarmnm@gmail.com>
2021-03-20 21:35:37 +0530

    Added login API

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -29,9 +29,9 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
         return user
 
-    def update(self, instance, validate_data):
+    def update(self, instance, validated_data):
         """Handle updating user account"""
-        if 'password' in validate_data:
+        if 'password' in validated_data:
             password = validated_data.pop('password')
             instance.set_password(password)
         return super().update(instance, validated_data)
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,8 +2,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status # handy HTTP status code to return from API
 from rest_framework import viewsets
-from rest_framework.authentication import TokenAuthentication
-from rest_framework import filters
+from rest_framework.authentication import TokenAuthentication # Permission
+from rest_framework import filters # Search DB
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from profiles_api import serializers
 from profiles_api import models
@@ -103,3 +105,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,) # Add permission function to be called for each request
     filter_backends = (filters.SearchFilter,) # filter backets 1 or more
     search_fields = ('name', 'email') # search fields
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authenticatoin tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
